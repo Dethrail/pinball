7f4e5662a5ce9bbdee2f07c50e795973b12ac687
Dethrail <max.od.ua@gmail.com>
2015-09-10 12:50:59 +0300

    ашч згдд ызкштп

--- Assets/scripts/board/PullSpring.cs
+++ Assets/scripts/board/PullSpring.cs
@@ -4,59 +4,32 @@ using System.Collections;
 public class PullSpring:MonoBehaviour
 {
 	public string inputButtonName = "Fire1";
-	public float distance = 50;
-	public float speed = 1;
 	public GameObject _ball;
 	public float Acceleration = 500;
+	public bool Ready = false;
+	public bool Fire = false;
 
-	public bool ready = false;
-	public bool fire = false;
-	public float followedDistance = 0;
-
-	void OnCollisionEnter(Collision other)
+	private void OnCollisionEnter(Collision other)
 	{
 		_ball = other.gameObject;
-		//if(other.gameObject.tag == "Ball") {
-		ready = true;
-		//}
+		Ready = true;
 	}
 
-	void OnCollisionexit(Collision other)
+	private void OnCollisionexit(Collision other)
 	{
 		_ball = null;
-		//if(other.gameObject.tag == "Ball") {
-		ready = false;
-		//}
+		Ready = false;
 	}
 
-	void Update()
+	private void Update()
 	{
+		Fire = Input.GetButton(inputButtonName);
 
-		if(Input.GetButton(inputButtonName)) {
-			//As the button is held down, slowly move the piece
-			if(followedDistance < distance) {
-				transform.Translate(0, 0, -speed * Time.deltaTime);
-				followedDistance += speed * Time.deltaTime;
-				fire = true;
-			}
-		} else if(followedDistance > 0) {
-			//Shoot the ball
-			if(fire && ready) {
-				_ball.transform.TransformDirection(Vector3.forward * 10);
-				_ball.GetComponent<Rigidbody>().AddForce(Vector3.forward * (Acceleration * followedDistance / distance), ForceMode.Acceleration);
-				fire = false;
-				ready = false;
-			}
-			//Once we have reached the starting position fire off!
-			transform.Translate(0, 0, 20 * Time.deltaTime);
-			followedDistance -= 20 * Time.deltaTime;
+		if(Fire && Ready) {
+			_ball.transform.TransformDirection(Vector3.forward * 10);
+			_ball.GetComponent<Rigidbody>().AddForce(Vector3.forward * Acceleration, ForceMode.Acceleration);
+			Fire = false;
+			Ready = false;
 		}
-
-		//Just ensure we don't go past the end
-		if(followedDistance <= 0) {
-			fire = false;
-			followedDistance = 0;
-		}
-
 	}
 }
